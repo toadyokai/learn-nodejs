b11f126898daec877a46cd513bfdeb949bbc79db
hanfei <hanfei@hanfeideMacBook-Pro.local>
2023-07-03 15:15:13 +0800

    docs: init chapter

---
+++ apps/chapter9-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'chapter9-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/chapter9-e2e',
+};
